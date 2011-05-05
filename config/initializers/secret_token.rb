3eb5ae213afec7d46b63bff95bf881fcf58e5a5c
Nick <nick@laptopper.(none)>
2011-05-04 22:15:03 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sbo::Application.config.secret_token = '74e89d9d66a6aac8a78123c62f463145a2fb15959a60c672542eea3faa3c563631edf37f6cafa99b668789700c69b9169aae0bdf031306f168fb8577e74e38ba'
